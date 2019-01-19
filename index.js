2692dc1c6f029b5aebf220700258354470eef0da
diptigandhi24 <gandhidipti.s@gmail.com>
2019-01-19 17:10:07 +0530

    Onclick the modal button, create a modalContainer element and append it to the overlaywrapper which currently the entire body

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+//Todo
+/*
+1.Create a simple modal
+2.Allow developer to give the custom width and height of the modal
+*/
 class overlay {
+  constructor() {
     this.modalBtn = document.getElementsByClassName("modal");
+
+    this.createModalContainer = () => {
+      let modalContainer = document.createElement("div");
+      modalContainer.classList.add("overlay");
+      return modalContainer;
+    };
+    this.openModal = () => {
+      document
+        .getElementById("overlayWrapper")
+        .appendChild(this.createModalContainer());
     };
     this.modalBtn[0].addEventListener("click", this.openModal);
   }
