8c2934dd2b5e5c5e7e2ecc8ef364d65a3bc0bb29
diptigandhi24 <gandhidipti.s@gmail.com>
2019-01-19 17:06:57 +0530

    Select a button and add eventListener  click

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 class overlay {
-  constructor(width = 200, height = 400) {
-    this.width = width;
-    this.height = height;
-    console.log("Width and Height", this.width, this.height);
-    this.test = () => {
-      console.log("Helloe you clicked me");
+    this.modalBtn = document.getElementsByClassName("modal");
     };
+    this.modalBtn[0].addEventListener("click", this.openModal);
   }
 }
 
