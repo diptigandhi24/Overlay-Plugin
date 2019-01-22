4659b5ccf800d711e6d090a457f9dbf0722e5c5d
diptigandhi24 <gandhidipti.s@gmail.com>
2019-01-22 17:07:28 +0530

    Till now modal is created using default setting, content is in html file,

--- index.js
+++ index.js
@@ -4,14 +4,42 @@
 2.Allow developer to give the custom width and height of the modal
 */
 class overlay {
-  constructor() {
+  //default modal setting
+  constructor({
+    closeBtn = false,
+    content = "TEST",
+    width = "100%",
+    height = "100%"
+  }) {
     this.modalBtn = document.getElementsByClassName("modal");
     this.modalContainer = document.createElement("div");
     this.modalContainer.classList.add("overlay");
+    this.closeBtn = closeBtn;
+    this.content = content;
+    this.width = width;
+    this.height = height;
+    console.log(
+      "variables with default arg",
+      this.closeBtn,
+      this.content,
+      this.width,
+      this.height
+    );
+    //open the modal on button click
     this.openModal = () => {
-      document
-        .getElementById("overlayWrapper")
-        .appendChild(this.modalContainer);
+      this.modalWrapper = document.createDocumentFragment();
+      this.modalWrapper.appendChild(this.modalContainer);
+      document.getElementById("overlayWrapper").appendChild(this.modalWrapper);
+      //check for the user setting to open the modal
+      if (this.closeBtn) {
+        this.closeX = document.createElement("button");
+        console.log("created button is", closeBtn, typeof closeBtn);
+        this.closeX.innerHTML = "X";
+        this.modalContainer.appendChild(this.closeX);
+      }
+      if (this.content) {
+        this.modalContainer.appendChild(this.content);
+      }
     };
 
     Object.keys(this.modalBtn).forEach(el => {
@@ -19,6 +47,11 @@ class overlay {
     });
 
     this.closeModal = () => {
+      //   this.modalWrapper.removeChild(this.modalContainer);
+      console.log("modal Container", this.modalContainer);
+      console.log("modal Wrapper", this.modalWrapper);
+      this.modalContainer.removeChild(this.closeX);
+      //   this.modalWrapper.parentNode.removeChild(this.modalContainer);
       document
         .getElementById("overlayWrapper")
         .removeChild(this.modalContainer);
@@ -27,4 +60,4 @@ class overlay {
   }
 }
 
-new overlay();
+new overlay({ content: document.getElementById("content"), closeBtn: true });
