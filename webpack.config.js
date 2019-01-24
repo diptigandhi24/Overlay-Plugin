97475c0c829aadd66c341e6282ba8657b516f39f
diptigandhi24 <gandhidipti.s@gmail.com>
2019-01-24 11:13:08 +0530

    Needed proper babel support for all the es6 features

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    bundle: ["babel-polyfill", "./src/index.js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }
+    ]
+  }
+};
